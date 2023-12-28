dbaa402625c843726fbb75d26a7a1e3cb33adc58
danieljml <danielgarrido425@gmail.com>
2023-12-27 22:52:31 -0400

    Adding chrono react lib for work experiencie

--- src/sections/work_experiencie/WorkExperiencie.tsx
+++ src/sections/work_experiencie/WorkExperiencie.tsx
@@ -1,12 +1,14 @@
-import Qualification from '../qualification/Qualification';
-import { Container } from '../../components';
+import { Container, Title } from '../../components';
+import { useTranslation } from 'react-i18next';
+import { Chrono } from 'react-chrono';
+import styled from 'styled-components';
 
 type WorkExperiencieProps = {
   workExperiencieList: {
     id: number;
     title: string;
-    date: string;
-    description: string;
+    cardSubtitle: string;
+    cardDetailedText: string;
     icon?: string;
     link?: string;
     position?: string;
@@ -16,14 +18,95 @@ type WorkExperiencieProps = {
 const WorkExperiencieSection = ({
   workExperiencieList,
 }: WorkExperiencieProps) => {
+  const { t } = useTranslation();
+
+  const workExperiences = workExperiencieList.map(items => {
+    return {
+      ...items,
+      cardDetailedText: t(items.cardDetailedText),
+    };
+  });
+
   return (
     <Container>
-      <Qualification
-        title="profile_summary.sections.work_background.heading"
-        qualification={workExperiencieList}
-      />
+      <WorkExperienceContainer>
+        <Title
+          content={t('profile_summary.sections.work_background.heading')}
+        />
+        <Chrono
+          items={workExperiences}
+          mode="VERTICAL_ALTERNATING"
+          hideControls
+          allowDynamicUpdate
+          disableAutoScrollOnClick
+          disableClickOnCircle
+          activeItemIndex
+          theme={{
+            primary: 'black',
+          }}
+          classNames={{
+            card: 'card',
+            cardTitle: 'card-title',
+            cardSubTitle: 'card-subtitle',
+            cardText: 'card-text',
+            title: 'title',
+          }}
+        />
+      </WorkExperienceContainer>
     </Container>
   );
 };
 
 export default WorkExperiencieSection;
+
+const WorkExperienceContainer = styled.div`
+  width: 1185px;
+
+  header {
+    padding: 0;
+  }
+
+  span {
+    background-color: ${({ theme }) => theme?.colors?.backgroundBody};
+  }
+
+  h1 {
+    text-align: start;
+    font-size: 25px;
+  }
+
+  .timeline-main-wrapper {
+    padding: 0;
+  }
+
+  .card {
+    border: none;
+    padding: 30px;
+    height: auto;
+    min-height: unset;
+    row-gap: 10px;
+    box-shadow: 0px 0px 25px rgb(0 0 0 / 18%);
+    background-color: ${({ theme }) => theme?.colors?.backgroundBody};
+    border-radius: 5px;
+  }
+
+  .title,
+  .card-text p,
+  .card-text,
+  .card-subtitle,
+  .card-title {
+    margin: 0;
+    padding: 0;
+    color: ${({ theme }) => theme?.colors?.primary};
+  }
+
+  .card-subtitle {
+    margin-top: 5px;
+  }
+
+  @media (max-width: 1185px) {
+    h1 {
+      text-align: center;
+    }
+  }
+`;
